11ae59822cd7020118f6240229407c21085fc9eb
Anonym xxx <oleksandrsoltis365@gmail.com>
2024-04-28 02:11:29 +0300

    createProfile

--- src/pages/personalCabinetWhoHelp/ProfileWho.tsx
+++ src/pages/personalCabinetWhoHelp/ProfileWho.tsx
@@ -6,11 +6,19 @@ const ProfileWho = () => {
     <>
  
 
- <div className="flex flex-grow   justify-between py-4 px-20 md:px-6 2xl:px-11">
-        {/* channel photo */}
-              <div className="relative -ml-[18%] bg-[#5BC0EB] w-[55rem] h-[55rem] rounded-full   dark:bg-primary  mr-3">
-                  <div className="relative top-[120px] left-[47%]"> <img
-                      className="rounded-full w-60 h-60"
+          <div className="  py-4 px-20 md:px-6 2xl:px-11">
+              {/* channel photo */}
+              <h1 className="text-[2rem] mr-[58rem]">Обліковий запис</h1>
+              <div className="flex flex-grow items-center justify-flex-start py-4 px-20 md:px-6 2xl:px-11">
+
+                  <a href="#" className="font-medium text-[20px] mx-4 text-black-600 underline dark:text-black-500 hover:no-underline">Налаштування</a>
+                  <a href="#" className="font-medium text-[20px] text-black-600 underline dark:text-black-500 hover:no-underline">Історя моїх платежів</a>
+                  <a href="#" className="font-medium text-[20px] mx-4 border-black  border-[2px] rounded-full px-5 text-black-600 underline dark:text-black-500 hover:no-underline">Допомогти</a>
+
+              </div>
+              <div className="relative -ml-[18%] bg-[#5BC0EB] w-[40rem] h-[40rem] rounded-full   dark:bg-primary  mr-3">
+                  <div className="relative top-[70px] left-[47%]"> <img
+                      className="rounded-full w-40 h-40"
                       src={Logo}
                       alt=""
                   />
@@ -21,17 +29,24 @@ const ProfileWho = () => {
                           <h1 className="text-black text-left font-bold my-8  text-3xl mb-2">Олександр Солти</h1>
 
                       </div>
+
+                      <button type="submit" className=" w-[267px] block    mt-12 px-2 py-2 bg-black border-[2px] border-[#5BC0EB]    rounded-[4px] text-center text-sm font-semibold text-[#5BC0EB] shadow-sm hover:bg-[#FEFCF4] hover:text-[#5BC0EB] hover:border-[#5BC0EB]   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Змінити пароль</button>
+
+                      <p className="font-semibold mt-24  text-[24px] font-medium leading-[28.78px] text-left">Видалити профіль</p>
+
+
+                  </div>  
+                  <div className="flex page mt-6 relative left-[59rem]">
+                      <Outlet></Outlet>
                   </div>
+              </div>
+
+
           </div>
-          
-          <div className="flex page mt-6">
-          <Outlet></Outlet>
-        </div>
-        </div>
-       
-      
-      
- 
+
+
+
+
     </>
   );
 };
