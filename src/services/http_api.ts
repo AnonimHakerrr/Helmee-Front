81a2f150d218558901beed7c03481f6a876cb68d
Anonym xxx <oleksandrsoltis365@gmail.com>
2024-04-28 22:14:34 +0300

    api

--- src/services/http_api.ts
+++ src/services/http_api.ts
@@ -10,30 +10,30 @@ const http_api = axios.create({
  
 });
 
-// http_api.interceptors.response.use(
-//   (responce: any) => responce,
-//   (error: AxiosError) => {
-//     console.log('catched', error);
-//     switch (error.response?.status) {
-//       case 401: {
-//         handleError('Please, sign in to act');
-//         break;
-//       }
-//       case 403: {
-//         handleError('Forbidden action');
-//         break;
-//       }
-//       case 422: {
-//         console.log('422', error);
-//         handleError(error);
-//         break;
-//       }
-//     }
-//     if (error.code == 'ERR_NETWORK') {
-//       window.location.href = '/auth/signin';
-//     }
-//     return Promise.reject(error);
-//   },
-// );
+http_api.interceptors.response.use(
+  (responce: any) => responce,
+  (error: AxiosError) => {
+    console.log('catched', error);
+    switch (error.response?.status) {
+      case 401: {
+        
+        break;
+      }
+      case 403: {
+       
+        break;
+      }
+      case 422: {
+        console.log('422', error);
+        
+        break;
+      }
+    }
+    if (error.code == 'ERR_NETWORK') {
+      window.location.href = '/auth/signin';
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default http_api;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+ 
+ 
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: 'localhost',
+    port: 5173,
+    proxy: {
+      // Proxy /api requests to your API server
+      '/api': {
+        target: '  http://198.46.226.156', // Replace with your API server's URL
+        changeOrigin: true,
+        secure: false,
+      },
+      '/signalr': {
+        target: ' http://198.46.226.156',
+        changeOrigin: true,
+        secure: false,
+        ws: true, // it`s important to use SignalR with Websockets as transport
+      },
+    },
+  },
+});
